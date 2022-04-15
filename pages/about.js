3419ad2f5fc42c94612702834d0bc1553525d5ff
Syed Ashraf <syedashraf.123.123@gmail.com>
2022-04-16 00:55:43 +0530

    css

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import styles from "../styles/About.module.css";
 function about() {
-  return <h1>about</h1>;
+  return (
+    <div>
+      <h1 className={styles.yellow}>about</h1>
+      <h2 className={styles.blue}>This is simple text</h2>
+      <h1 className={`${styles.blue} ${styles["font-size-50"]}`}>
+        Two Classes
+      </h1>
+      <h1 className={`${styles.blue} ${styles.font}`}>Font of two classes</h1>
+      <h1 style={{ fontSize: "50px", color: "black", backgroundColor: "pink" }}>
+        This a random text
+      </h1>
+    </div>
+  );
 }
 
 export default about;
--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,24 @@
 import React from "react";
 import Home from "../components/Home";
+import styles from "../styles/Home.module.css";
 function home() {
-  return <Home></Home>;
+  return (
+    <div>
+      <style jsx>{`
+        .color {
+          color: blue;
+          font-size: 150px !important;
+        }
+        .color-2 {
+          color: red;
+        }
+      `}</style>
+      <div className={styles.font}>
+        <Home></Home>
+        <span className="color color-2"> Heyy</span>
+      </div>
+    </div>
+  );
 }
 
 export default home;
