b03ff7c1852a6908fef79f4e1ab55c42bd2a7412
Syed Ashraf <syedashraf.123.123@gmail.com>
2022-04-16 01:26:51 +0530

    Revert "head"
    This reverts commit f0eeafb6d05e325d52364269bb55eaa6510cc949.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
+import Image from "next/image";
 export default function Home() {
-  return <h1>Index</h1>;
+  return (
+    <>
+      <h1>Index Page</h1>
+
+      <img src="/1.jpg" alt="This is a tree" />
+      <Image
+        src="/1.jpg"
+        height="500px"
+        width="500px"
+        alt="This is a tree"
+        quality={50}
+        priority={true}
+      />
+      {/* <Image src="/1.jpg" layout="fill" /> */}
+    </>
+  );
 }
