7152d90d51c5a4014a2e088ca84143d7efa3c4ef
Syed Ashraf <syedashraf.123.123@gmail.com>
2022-04-18 01:25:08 +0530

    changedUrl

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack5: true,
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,25 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 export default function Home({ res }) {
-  const [user, setUser] = useState(res[0]);
+  const [user, setUser] = useState(res.results);
 
   return (
     <>
-      <h1>
-        {user ? (
-          <>
-            {user.name} {user.age}
-          </>
-        ) : (
-          "Loading..."
-        )}
-      </h1>
+      {user.map((item, idx) => {
+        return (
+          <div key={idx}>
+            <p>
+              {idx} -- {item.question}
+            </p>
+            <p>{item.correct_answer}</p>
+          </div>
+        );
+      })}
     </>
   );
 }
 
 export async function getServerSideProps() {
-  const response = await fetch("http://localhost:5000");
+  const response = await fetch("https://opentdb.com/api.php?amount=10");
   const res = await response.json();
   return { props: { res } };
 }
